3bdb07a00bee24d25feba61b01c7df21bcb47f3a
NicholasLabati <labati.nicholas.luigi@iisgalvanimi.edu.it>
2021-04-20 07:48:25 +0000

    Finito

---
+++ client-socketio/src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { SocketService } from './socket.service';
+import { Observable } from 'rxjs';
+import { CesarService } from './cesar.service';
+import { FormData } from './form/data.model';
+import { CyptoJsService } from './crypto.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  messageList: string[] = [];
+  obs: Observable<any>;
+  dfdf: string;
+  message: string;
+  constructor(private socketService: SocketService, private cesarService: CesarService, private cryptoService: CyptoJsService) {
+  }
+
+  sendKey(kay: HTMLInputElement) {
+    this.dfdf = kay.value;
+  }
+
+ sendMessage(formData: FormData) {
+    console.log("form input: " + JSON.stringify(formData));
+
+    let encoded: FormData = formData; //Preparo una variabile da criptare
+    switch (formData.messageType) {
+      //Se il tipo di messaggio è cesar allora cripto con cesarService
+      case "cesar":
+        encoded.message = this.cesarService.encode(formData.message, Number(this.dfdf));
+        break;
+      //Se il tipo di messaggio è t-des allora cripto con cryptoService.encodeDes
+      case "t-des":
+        encoded.message = this.cryptoService.encodeDes(formData.message, this.dfdf);
+        break;
+    }
+    //Invio il messaggio cifrato
+    this.socketService.sendMessage(JSON.stringify(encoded));
+
+    this.message = "";
+  }
+  decodeData = (messageData: string) => {
+    let received: FormData = JSON.parse(messageData);
+    console.log("messagereceived: " + JSON.stringify(received))
+
+    switch (received.messageType) {
+      case "cesar":
+        received.message = this.cesarService.decode(received.message, Number(this.dfdf));
+        break;
+
+      case "t-des":
+        received.message = this.cryptoService.decodeDes(received.message, this.dfdf);
+        break;
+    }
+
+    this.messageList.push("messaggio cifrato: " + messageData + " messaggio decifrato " + JSON.stringify(received));
+
+  }
+  ngOnInit() {
+    this.obs = this.socketService.getMessage();
+    this.obs.subscribe(this.decodeData);
+  }
+}
